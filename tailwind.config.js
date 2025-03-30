8e0408dcc8be9f68446de9bcd00322d3e314c2e2
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2025-03-30 19:22:10 +0530

    Added tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:["class"],
     content: [
       "./app/**/*.{js,ts,jsx,tsx,mdx}",
       "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,8 +9,28 @@ module.exports = {
       // Or if using "src" directory:
       "./src/**/*.{js,ts,jsx,tsx,mdx}",
     ],
+    prefix:"",
     theme: {
-      extend: {},
+      container:{
+        center: true,
+        padding: "15px",
+       
+      },
+      screen:{
+        sm:"640px",  
+        md:"768px", 
+        lg:"960px", 
+        xl:"1200px", 
+      },
+      extend: {
+        keyframe:{
+          "according-down":{
+            from:{height:"0"},
+            to:{height:"var(--radix-according-content-height)"},
+          }
+        },
+        "a"
+      },
     },
     plugins: [],
   };
