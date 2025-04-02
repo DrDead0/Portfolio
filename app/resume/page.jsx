baf75150ad3735f0a2a2469dde0ed6dde35242a2
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2025-04-02 20:19:58 +0530

    Added Services,work,resume,contact pages

---
+++ app/resume/page.jsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+
+
+
+const Resume =()=>{
+    return(
+        <div>Resume Pages</div>
+    )
+}
+export default Resume;
---
+++ app/work/page.jsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+
+
+
+const work =()=>{
+    return(
+        <div>Work Pages</div>
+    )
+}
+export default work;
