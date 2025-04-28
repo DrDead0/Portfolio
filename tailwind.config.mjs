b7db37f2fe3d63c9639853dd7c4944c885e26c81
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2025-04-28 05:33:38 +0530

    Added tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
+export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,26 +8,23 @@ const config = {
   theme: {
     extend: {
       colors: {
-        lightHover:'#fcf4ff',
+        lightHover: '#fcf4ff',
         darkHover: '#2a004a',
         darkTheme: '#11001F',
       },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"]
+      },
       boxShadow: {
-        'black': '4px 4px 0px #000000',
-        'white': '4px 4px 0px #ffffff',
+        'black' : '4px 4px 0 #000',
+        'white' : '4px 4px 0 #fff',
       },
-      fontFamily:{
-        Outfit: ['Outfit', 'sans-serif'],
-        Ovo: ['Ovo', 'serif'],
-      },
-      gridTemplateColumns:{
-        'auto': 'repeat(auto-fit, minmax(200px,1fr))',
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
       }
-
     },
   },
   darkMode: 'selector',
   plugins: [],
 };
-
-export default config; 
