1225163b38c34a67e34245ae6f08db4ecf00bdae
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2025-04-28 05:33:00 +0530

    Added more fearture:

--- app/layout.js
+++ app/layout.js
@@ -1,31 +1,29 @@
-import { Outfit, Ovo } from "next/font/google";
+import { Outfit, Ovo} from "next/font/google";
 import "./globals.css";
 
 const outfit = Outfit({
-  subsets: ["latin"],
-  weight:["400","500","600","700"],
+  subsets: ["latin"], weight: ["400", "500", "600", "700"]
 });
 
 const ovo = Ovo({
-  subsets: ["latin"],
-  weight:["400"],
+  subsets: ["latin"], weight: ["400"]
 });
 
+
+
 export const metadata = {
-  title: "Portfolio",
-  description: "Ashish Chaurasiya's Portfolio",
+  title: "Portfolio - Ashish Chaurasiya",
+  description: "Game & Backend Developer From Uttar Pradesh, India doing Multiple Things Like Game Development, Backend Development, and also Open Source Contributor.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth ">
+    <html lang="en" className="scroll-smooth">
       <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-8
-          overflow-x-hidden `}
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
       >
         {children}
       </body>
     </html>
   );
 }
-// export default Navbar;
--- app/page.js
+++ app/page.js
@@ -1,22 +1,44 @@
-"use client";
-import Image from "next/image";
-import Navbar from "./components/navbar.jsx";
-import Header from "./components/header.jsx";
-import About from "./components/about.jsx";
-import Services from "./components/services.jsx";
-import Work from "./components/work.jsx";
-import Contact from "./components/contact.jsx";
-import Footer from "./components/footer.jsx";
+'use client'
+import { useEffect, useState } from "react";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Services from "../components/Services";
+import Work from "../components/Work";
+
 export default function Home() {
+
+ const [isDarkMode, setIsDarkMode] = useState(false);
+
+ useEffect(()=>{
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    setIsDarkMode(true)
+  }else{
+    setIsDarkMode(false)
+  }
+ },[])
+
+ useEffect(()=>{
+    if(isDarkMode){
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    }else{
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = '';
+    }
+ },[isDarkMode])
+
   return (
     <>
-      <Navbar/>
-      <Header/>
-      <About/>
-      <Services/>
-      <Work/>
-      <Contact/>
-      <Footer/>
+    <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
+    <Header isDarkMode={isDarkMode} />
+    <About isDarkMode={isDarkMode} />
+    <Services isDarkMode={isDarkMode} />
+    <Work isDarkMode={isDarkMode} />
+    <Contact isDarkMode={isDarkMode} />
+    <Footer isDarkMode={isDarkMode} />
     </>
   );
 }
